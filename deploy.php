44587942353a4546f4cf5aa5015f56a49ae05b6d
purushothamjsn <purushotham@jumpstartninja.com>
2017-10-21 12:26:15 +0530

    deploy file updated

--- deploy.php
+++ deploy.php
@@ -4,7 +4,7 @@
 	 *
 	 * Used for automatically deploying websites via github or bitbucket, more deets here:
 	 *
-	 *		https://gist.github.com/1809044
+	 *		https://gist.github.com/purushothamjsn/jsndemo
 	 */
 
 	// The commands
@@ -30,21 +30,3 @@
 
 	// Make it pretty for manual user access (and why not?)
 ?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
-
-<?php echo $output; ?>
-</pre>
-</body>
-</html>
